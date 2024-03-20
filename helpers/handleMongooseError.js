7356c7783c40e9dff80981bda7ba51fd85099547
Anastasiia-Rodzina <pototzkaja.n@gmail.com>
2024-03-20 10:58:54 +0200

    correct import handleMongooseError

--- helpers/handleMongooseError.js
+++ helpers/handleMongooseError.js
@@ -1,4 +1,5 @@
-export const handleMongooseError = (error, data, next) => {
+const handleMongooseError = (error, data, next) => {
   error.status = 400;
   next();
 };
+export default handleMongooseError;
